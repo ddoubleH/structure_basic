d099501db13a966bf3a8c9d439756a2b71fb6a96
ddoubleH <whh1015@gmail.com>
2019-08-13 11:14:00 +0900

    Update linkedlist.py
    1. Delete 함수 구현
      - 값을 기준으로 삭제 (중복 없는 상황)

--- linkedlist.py
+++ linkedlist.py
@@ -3,13 +3,17 @@ class Node:
         self.data = None
         self.link = None
 
+
 class LinkedList:
     state = True
 
     def __init__(self):
         self.head = Node()
+        self.before = None
+        self.current = None
         self.tail = self.head
         self.nodelist = [self.head]
+        self.nodenum = 1
 
     def newNode(self, data):
 
@@ -22,14 +26,27 @@ class LinkedList:
     # 추가 (노드와 노드 사이에 데이터 추가)
     def insert(self, LinkedList, newnode):
 
+        # 헤드 뒤 연결
         if LinkedList.state:
             LinkedList.head.link = newnode
+            LinkedList.nodelist.append(newnode)
+            #LinkedList.current = newnode
+            #LinkedList.before = LinkedList.head
             LinkedList.state = False
-        else:
-            LinkedList.tail.link = newnode
 
-        LinkedList.nodelist.append(newnode)
-        LinkedList.tail = newnode
+        # 노드와 노드 사이에 데이터 추가
+        # 값, 인덱스
+        elif False:
+            pass
+
+        # 젤뒤에 연결
+        else:
+            #LinkedList.before = LinkedList.nodelist[-1]
+            #LinkedList.current = newnode
+            LinkedList.tail = newnode
+            LinkedList.nodelist[-1].link = newnode
+            LinkedList.nodelist.append(newnode)  # 뒤에 붙임
+
 
     def info(linkedlist):
         print('LinkedList Head : ', linkedlist.head,
@@ -37,13 +54,48 @@ class LinkedList:
 
         for i in range(len(linkedlist.nodelist)):
             print(i, 'Node :', linkedlist.nodelist[i],
-                  i, 'Node Data : ' , linkedlist.nodelist[i].data,
-                  i, 'Node Link : ', linkedlist.nodelist[i].link )
+                  i, 'Node Data : ', linkedlist.nodelist[i].data,
+                  i, 'Node Link : ', linkedlist.nodelist[i].link)
 
-    # 삭제 (맨뒤에 있는 노드, 중간에 노드 > 값 )
+    def isEmpty(self, LinkedList):
+        if LinkedList is None:
+            return True
+        else:
+            return False
+
+    # 삭제 (중복 값 없음)
+    # Value, Index
+    def delete(self, LinkedList, removeData):
+        if self.isEmpty(LinkedList):
+            return 'No node to delete'
+
+        # 지우고자 하는 값이 Tail인 경우
+        elif LinkedList.nodelist[-1].data == removeData:
+            LinkedList.tail = LinkedList.nodelist[-2]
+            LinkedList.nodelist[-2].link = None
+            del LinkedList.nodelist[-1]
+            return 'Node Remove'
+
+        # 지우고자 하는 값이 중간에 있는 경우
+        else:
+            for i in range(len(LinkedList.nodelist)):
+                if LinkedList.nodelist[i].data == removeData:
+                    # swap
+                    LinkedList.nodelist[i-1].link = LinkedList.nodelist[i].link
+                    del LinkedList.nodelist[i]
+                    return 'Node Remove'
+        
 
     # 검색 (몇번째 노드인지 출력)
-
+    def search(self, LinkedList, node):
+        if LinkedList.tail == None:
+            print('No node to search')
+            return
+        else:
+            link = LinkedList.head.link
+            num = 0
+            if node == data:
+                return num
 
 
 if __name__ == '__main__':
@@ -52,13 +104,22 @@ if __name__ == '__main__':
     while(True):
         word = input('Input Command > ')
 
-        if word == 'quit' or word == 'q' :
+        if word == 'quit' or word == 'q':
             break
 
         elif word == 'insert':
             data = input('Input Node Data > ')
             node = LinkedList.newNode(data)
             LinkedList.insert(LinkedList, node)
+
+        elif word =='delete':
+            data = input('Delete Node Data > ')
+            LinkedList.delete(LinkedList, removeData=data)
+
+        elif word =='search':
+            data = input('Search Node Data > ')
+            node = LinkedList.newNode(data)
+            LinkedList.search(LinkedList, node)
 
         elif word == 'info':
             LinkedList.info()
