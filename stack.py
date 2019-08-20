800d1e663bb50a3320d151be6b181c5ee42fb9d8
ddoubleH <whh1015@gmail.com>
2019-08-20 19:32:05 +0900

    Update stack.py
    1. Stack 구현
       -push
       -pop

--- stack.py
+++ stack.py
@@ -1,17 +1,35 @@
-
+#Last in first out
 class stack():
 
 	def __init__(self):
-		pass
+		self.stack = []
 
-	def psuh(self):
-		pass
+	def push(self, data):
+		self.stack.append(data)
 
 	def pop(self):
-		pass
+		del self.stack[-1]
+		return
 
-	def isEmpty(self):
-		pass
+	def isEmpty(stack):
+		if stack is None:
+			return True
+		else:
+			return False
 
 	def getSize(self):
-		pass
+		return len(self.stack)
+
+
+if __name__ == '__main__':
+	S = stack()
+	print(S.stack)
+	S.push(10)
+	S.push(20)
+	S.push(30)
+	S.push(40)
+	S.push(50)
+	print(S.stack)
+	S.pop()
+	S.pop()
+	print(S.stack)
