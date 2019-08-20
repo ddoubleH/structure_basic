bb90f55b3b433d5f0dc52511c0abb061e9733d34
ddoubleH <whh1015@gmail.com>
2019-08-20 19:25:12 +0900

    Update queue.py
    1. queue 구현
       -enqueue
       -dequeue

--- queue.py
+++ queue.py
@@ -1,15 +1,33 @@
 class queue():
 	def __init__(self):
-		pass
+		self.queue = []
 
-	def inqueue(self):
-		pass
+	def enqueue(self, data):
+		self.queue.append(data)
 
 	def dequeue(self):
-		pass
+		del self.queue[0]
+		return
 
-	def isEmpty(self):
-		pass
+	def isEmpty(self, queue):
+		if queue is None:
+			return True
+		else:
+			return False
 
 	def getSize(self):
-		pass
+		return len(self.queue)
+
+
+if __name__ == '__main__':
+	Q = queue()
+	print(Q.queue)
+	Q.enqueue(10)
+	Q.enqueue(20)
+	Q.enqueue(30)
+	Q.enqueue(40)
+	Q.enqueue(50)
+	print(Q.queue)
+	print(Q.dequeue())
+	print(Q.dequeue())
+	print(Q.queue)
